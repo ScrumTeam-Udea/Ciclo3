67a6dd481e0a5b371d239ad19d3f791bc51bec24
Juan Gil <gilj06@hotmail.com>
2022-08-27 22:41:36 +0200

    Modelado_Empresa

---
+++ src/main/java/com/ScrumTeam/Proyecto/MinTic/Modelado_Empresa/MovimientoDinero.java
@@ -0,0 +1,39 @@
+package com.ScrumTeam.Proyecto.MinTic.Modelado_Empresa;
+
+public class MovimientoDinero {
+    private int monto;
+    private String concepto;
+    private Empleado usuario;
+
+    //constructor
+    public MovimientoDinero(int monto, String concepto, Empleado usuario) {
+        this.monto = monto;
+        this.concepto = concepto;
+        this.usuario = usuario;
+    }
+
+    //getters y setters
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public Empleado getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Empleado usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ src/test/java/com/ScrumTeam/Proyecto/MinTic/ProyectoMinTicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScrumTeam.Proyecto.MinTic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoMinTicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
