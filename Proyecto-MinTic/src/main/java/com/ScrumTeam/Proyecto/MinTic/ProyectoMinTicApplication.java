6fc49ee03b53f401150bc7db6323efc100c263bf
Juan Esteban Gil Martínez <111530180+juan-gil-05@users.noreply.github.com>
2022-08-27 22:23:11 +0200

    Modelado de clases
    clases: Empresa, Empleado, MovimientoDinero

---
+++ Proyecto-MinTic/src/main/java/com/ScrumTeam/Proyecto/MinTic/ProyectoMinTicApplication.java
@@ -0,0 +1,13 @@
+package com.ScrumTeam.Proyecto.MinTic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMinTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMinTicApplication.class, args);
+	}
+
+}
