c2697c27aacf9b494c219fa279397344c519b5de
Laura Ramirez-Ramirez <110516122+LauraRamirez-Ramirez@users.noreply.github.com>
2022-08-28 22:07:59 -0500

    Actualización clase empresa

--- src/main/java/com/ScrumTeam/Proyecto/MinTic/Modelado_Empresa/Empresa.java
+++ src/main/java/com/ScrumTeam/Proyecto/MinTic/Modelado_Empresa/Empresa.java
@@ -1,23 +1,42 @@
 package com.ScrumTeam.Proyecto.MinTic.Modelado_Empresa;
 
 public class Empresa {
+    private int id;
     private String nombre;
-    private String direccion;
-    private String telefono;
     private String nit;
+    private String telefono;
+    private String direccion;
+    
+    private List<Empleado> empleados = new ArrayList<>();
 
+    private List<MovimientoDinero> transacciones = new ArrayList<>();
+    
     //constructor
     public Empresa(String nombre, String direccion, String telefono, String nit) {
+       
+        super();
+        this.id = id;
         this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = telefono;
         this.nit = nit;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.empleado = empleado;
+    }
+    
+    public void addEmpleado (Empleado empleado) {
+        empleados.add(empleado);
+    }
+
+    public void removeEmpleado(Empleado empleado1) {
+        empleados.remove(empleado1)
     }
 
     //getters y setters
-    public String getNombre() {return nombre;}
+    public String getNombre() {
+        return nombre;}
 
-    public void setNombre(String nombre) {this.nombre = nombre;}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;}
 
     public String getDireccion() {
         return direccion;
