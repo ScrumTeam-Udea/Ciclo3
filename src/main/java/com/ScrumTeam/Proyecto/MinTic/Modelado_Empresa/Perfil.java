43146d42236b97bc35f40b5e364529269e5484a8
Andres Sanchez <fandressanchezm@gmail.com>
2022-09-01 08:23:40 -0500

    Generacion de la clase perfil

--- src/main/java/com/ScrumTeam/Proyecto/MinTic/Modelado_Empresa/Perfil.java
+++ src/main/java/com/ScrumTeam/Proyecto/MinTic/Modelado_Empresa/Perfil.java
@@ -1,4 +1,40 @@
 package com.ScrumTeam.Proyecto.MinTic.Modelado_Empresa;
 
 public class Perfil {
+
+    private long id;
+    private String image;
+    private String telefono;
+
+
+    public Perfil(long id, String image, String telefono) {
+        this.id = id;
+        this.image = image;
+        this.telefono = telefono;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
